2e799d87df0c32db9df84e8a029888cc6071abf2
zooyaho <jiwoo1268@naver.com>
2025-07-16 17:55:32 +0900

    feat: 목표 추가 기능 및 ui 추가 > ScrollView사용

--- rn-my-goals-app/App.js
+++ rn-my-goals-app/App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCoursGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText);
+    setCoursGoals((prevCourseGoals) => [...prevCourseGoals, enteredGoalText]);
+    // setEnteredGoalText(""); // 초기화
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          value={enteredGoalText}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {/* ScrollView의 사용 가능한 높이를 제한하기 위해 View로 차지할 높이를 설정 */}
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals
+            .slice()
+            .reverse()
+            .map((goal, index) => (
+              <View key={"goal-" + index} style={styles.goalItem}>
+                <Text style={styles.goalText}>{goal}</Text>
+              </View>
+            ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    width: "70%",
+    padding: 10,
+    marginRight: 8,
+    borderRadius: 6,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    // borderWidth: 1,
+    // borderColor: "#cccccc",
+    backgroundColor: "powderblue",
+  },
+  goalText: {
+    color: "white", // font색상은 <Text>에서만 적용됨!
   },
 });
