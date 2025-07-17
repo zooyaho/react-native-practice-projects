aa9d7ffb25751eb717cca4a81649fb323210b118
zooyaho <jiwoo1268@naver.com>
2025-07-17 17:54:58 +0900

    feat: 시작화면 배경 퍼블리싱
    - expo-linear-gradient 패키지 사용
    - ImageBackground 컴포넌트 사용하여 배경에 이미지 적용

--- rn-quess-my-number-app/screens/StartGameScreen.js
+++ rn-quess-my-number-app/screens/StartGameScreen.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     elevation: 4, // Android shadow
     shadowColor: "black", // iOS shadow
     shadowOffset: { width: 0, height: 2 }, // iOS 그림자 위치
